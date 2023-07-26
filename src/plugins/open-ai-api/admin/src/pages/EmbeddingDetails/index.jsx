d637dc5d16c2e2e2f6ade69e7330ac56582aa840
Paul Bratslavsky <Paul.bratslavsky@gmail.com>
2023-07-26 10:46:15 -0500

    updated embeddings detail and added delete confirmation

--- src/plugins/open-ai-api/admin/src/pages/EmbeddingDetails/index.jsx
+++ src/plugins/open-ai-api/admin/src/pages/EmbeddingDetails/index.jsx
@@ -1,10 +1,67 @@
 // @ts-nocheck
 import React, { useEffect, useState } from "react";
-import { Box, Button } from "@strapi/design-system";
+import {
+  Box,
+  Flex,
+  Button,
+  Typography,
+  Dialog,
+  DialogBody,
+  DialogFooter,
+} from "@strapi/design-system";
+import { ExclamationMarkCircle, Trash } from "@strapi/icons";
 import { useFetchClient } from "@strapi/helper-plugin";
 import Header from "../../components/Header";
 import pluginId from "../../pluginId";
 import { useHistory, useParams } from "react-router-dom";
+import styled from "styled-components";
+
+const StyledTypography = styled(Typography)`
+  display: block;
+  margin-bottom: 1rem;
+`;
+
+function ConfirmDeleteEmbedding({ callback, isLoading }) {
+  const [isVisible, setIsVisible] = useState(false);
+
+  return (
+    <>
+      <Button onClick={() => setIsVisible(true)}>Deleting Embeddings</Button>
+      <Dialog
+        onClose={() => setIsVisible(false)}
+        title="Confirmation"
+        isOpen={isVisible}
+      >
+        <DialogBody icon={<ExclamationMarkCircle />}>
+          <Flex direction="column" alignItems="center" gap={2}>
+            <Flex justifyContent="center">
+              <Typography id="confirm-description">
+                Are you sure you want to delete this?
+              </Typography>
+            </Flex>
+          </Flex>
+        </DialogBody>
+        <DialogFooter
+          startAction={
+            <Button onClick={() => setIsVisible(false)} variant="tertiary">
+              Cancel
+            </Button>
+          }
+          endAction={
+            <Button
+              variant="danger-light"
+              onClick={callback}
+              startIcon={<Trash />}
+              disabled={isLoading}
+            >
+              {isLoading ? "Deleting Embeddings" : "Delete Embeddings"}
+            </Button>
+          }
+        />
+      </Dialog>
+    </>
+  );
+}
 
 export default function EmbeddingDetails() {
   const history = useHistory();
@@ -27,34 +84,29 @@ export default function EmbeddingDetails() {
     await del(`/open-ai-api/embeddings/delete-embedding/${id}`);
   };
 
-  async function handleSubmit(e) {
+  async function handleDelete(e) {
     e.preventDefault();
     e.stopPropagation();
     setIsLoading(true);
-    alert("Deleting");
     await deleteEmbeddings(params.id);
     history.push("/plugins/" + pluginId + "/");
   }
 
   return (
-    <div className="container">
+    <div>
       <Header
         link={"/plugins/" + pluginId + "/"}
-        title="Manage Embeddings"
-        subtitle="Manage your embeddings"
+        title={data.title || "Embeddings Details"}
+        subtitle={`Pinecone ID: ${data.embeddingsId}`}
       />
       <Box padding={8}>
-        <h1>Embeddings Details: {params.id}</h1>
-        <h2>{data.title}</h2>
-        <p>{data.content}</p>
-        <pre>{JSON.stringify(data.embeddings, null, 2)}</pre>
+        <Box padding={4} background="neutral0">
+          <StyledTypography variant="beta">Embeddings Content</StyledTypography>
+          <StyledTypography variant="omega">{data.content}</StyledTypography>
+        </Box>
       </Box>
       <Box padding={8}>
-        <form onSubmit={handleSubmit}>
-          <Button type="submit" disabled={isLoading}>
-            {isLoading ? "Deleting Embeddings" : "Delete Embeddings"}
-          </Button>
-        </form>
+        <ConfirmDeleteEmbedding callback={handleDelete} />
       </Box>
     </div>
   );
